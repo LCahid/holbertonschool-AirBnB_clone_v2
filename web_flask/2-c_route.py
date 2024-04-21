0330a7044c5fa5ff1db3a1293f2eba635b23d3c6
Cahid <mr.cebizade@gmail.com>
2024-04-21 18:29:33 +0400

    c route added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module for the flask application'''
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url.map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

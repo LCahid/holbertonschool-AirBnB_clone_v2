a535adc638af74ed18ef998e43fd29caf81b2cf5
Cahid <mr.cebizade@gmail.com>
2024-04-21 18:51:15 +0400

    changes added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,7 @@ class FileStorage:
                     self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
